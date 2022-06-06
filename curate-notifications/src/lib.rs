f4f2556e614f2543bfcedf58c02fce65c11379c3
m.gridin <msgridin@gmail.com>
2022-06-06 08:03:26 +0300

    .

--- curate-notifications/src/lib.rs
+++ curate-notifications/src/lib.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use chrono::{Timelike, Utc};
 use tokio::time::{Duration, interval};
 use sqlx::{Pool, Postgres};
 
@@ -11,14 +12,17 @@ pub async fn task(main_currencies: [&str; 3], server_api_key: &str, connection_s
 
     let db_pool = data::db::init(connection_string).await;
 
-    let mut interval = interval(Duration::from_secs(24 * 3_600));
+    let mut interval = interval(Duration::from_secs(3_600));
     loop {
         interval.tick().await;
-        let result = run_task(main_currencies, server_api_key, &db_pool).await;
-        match result {
-            Ok(_) => {},
-            Err(e) => println!("[SEND NOTIFICATIONS ERROR]{:?}", e)
-        };
+        let now = Utc::now();
+        if now.hour() >= 12 && now.hour() < 13 {
+            let result = run_task(main_currencies, server_api_key, &db_pool).await;
+            match result {
+                Ok(_) => {},
+                Err(e) => println!("[SEND NOTIFICATIONS ERROR]{:?}", e)
+            };
+        }
     }
 }
 
